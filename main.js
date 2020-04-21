cb617174aba77b0e685905a471c5260c381b93bf
Sunhound91 <32577426+Sunhound91@users.noreply.github.com>
2020-04-21 11:29:14 -0500

    committing index.html main.css main.js

--- main.js
+++ main.js
@@ -1,41 +1,51 @@
-var styleSheet= document.styleSheets;
-var body=document.getElementsByTagName('body');
-var wtBrd = document.getElementById('wtBrd');
-var intro=document.getElementById('intro');
-var prpBx = document.getElementById('prpBx');
-wtBrd.style.setProperty('visibility', 'hidden');
-intro.style.setProperty('visibility', 'hidden');
-prpBx.style.setProperty('visibility', 'hidden');
 
-console.log(wtBrd.id);
-function addProperty(n){
-   // element= styleSheet[0].cssRules[0].cssRules[n];
-    if(n==18){
-        wtBrd.style.setProperty('visibility', 'visible');
-        wtBrd.style.setProperty('animation-name','slideRight');
-    }
-    else if(n==19){
-        prpBx.style.setProperty('visibility', 'visible');
-        prpBx.style.setProperty('animation-name', 'holdPos');
-    }
-    else if(n==11){
-        intro.style.setProperty('visibility', 'visible');
-        intro.style.setProperty('animation-name', 'fadeIn');
-    }
-    //figure out workaround for changing n way to keep track
-    console.log('hello');
+var styleSheetRules= document.styleSheets[0].cssRules[0].cssRules;
+var slideRights= document.getElementsByClassName('slideRight');
+var holdPos=document.getElementsByClassName('holdPos');
+var fades=document.getElementsByClassName('fadeIn');
+ //hide elements on load//
+function hideElements(collctElem){
+  for(item of collctElem){
+    item.style.setProperty('visibility','hidden');
+  }
 }
-
-
-var bounding= prpBx.getBoundingClientRect();
-console.log(body);
-function startAnimation(){
-    var bounding=prpBx.getBoundingClientRect();
-    if(bounding.top <= 630){
-        addProperty(11);
-        addProperty(18);//create method to get index of specific elements or workaround because they change
-        addProperty(19);
-        this.console.log('hello');
-        window.removeEventListener('scroll',startAnimation);
-}}
-addEventListener('scroll', startAnimation);
+function isInViewPort(elem){
+  var bounding = elem.getBoundingClientRect();
+  return (
+       bounding.top >= 100 &&
+      // bounding.left >= 0 &&
+      bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight) 
+      // bounding.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+ function getAnimate(collctElem){
+  for(item of collctElem){
+      if(collctElem==fades){
+        if(isInViewPort(item)){
+          item.style.setProperty('visibility','visible');
+          item.style.setProperty('animation-name','fadeIn');
+        }
+      }
+      else if(collctElem==holdPos){
+        if(isInViewPort(item)){
+          item.style.setProperty('visibility','visible');
+          item.style.setProperty('animation-name','holdPos');
+        }
+      }
+      else if(collctElem==slideRights){
+        if(isInViewPort(item)){
+          item.style.setProperty('visibility','visible');
+          item.style.setProperty('animation-name','slideRight');
+        }
+    }
+  }
+}
+ hideElements(slideRights);
+ hideElements(holdPos);
+ hideElements(fades);
+ window.onscroll=function(){
+  getAnimate(slideRights);
+  getAnimate(holdPos);
+  getAnimate(fades);
+ };
+// animate elements on viewport location//
